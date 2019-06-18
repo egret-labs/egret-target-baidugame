69113eec16a19f4ae6622c22abc1734e8e7d0e90
fengtao <fengtao@egret.com>
2019-06-18 15:51:17 +0800

    Adapt WebTextMeasurer.ts

--- src/WebSysImpl.ts
+++ src/WebSysImpl.ts
@@ -138,6 +138,16 @@ namespace egret.baidugame {
     }
     egret.sys.drawTextureElements = drawTextureElements;
     
+    /**
+     * 测量文本的宽度
+     * @param context 
+     * @param text 
+     */
+    function measureTextWith(context: CanvasRenderingContext2D, text: string): number {
+        return context.measureText(text).width;
+    }
+    egret.sys.measureTextWith = measureTextWith;
+    
     egret.Geolocation = egret.baidugame.WebGeolocation;
     egret.Motion = egret.baidugame.WebMotion;
 }
